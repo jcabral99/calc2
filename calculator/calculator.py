cdbf7f9169fe6a21a673cd2315081fafdbea5a0a
jcabral99 <jc2447@njit.edu>
2021-11-13 18:42:35 -0500

    tests and history

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,13 +1,23 @@
 """This is the increment function"""
 # first import the addition namespace
 from Calc.addition import Addition
+from Calc.subtraction import Subtraction
+from Calc.multiplication import Multiplication
+from Calc.division import Division
 
 class Calculator:
     """This is the Calculator class"""
     history = []
     @staticmethod
+    def get_result_of_first_calculation_added_to_history():
+        return Calculator.history[0].getresult()
+    @staticmethod
+    def clear_history():
+        Calculator.history.clear()
+        return True
+    @staticmethod
     def history_count():
-        return Calculator.history.count()
+        return len(Calculator.history)
     @staticmethod
     def add_calculation_to_history(calculation):
         Calculator.history.append(calculation)
@@ -27,8 +37,21 @@ class Calculator:
     @staticmethod
     def subtract_number(value_a, value_b):
         """"subtract number from result"""
-        return value_a - value_b
+        # create an subtract object using the factory we created on the calculation class
+        subtraction = Subtraction.create(value_a, value_b)
+         # addition = addition(value_a, value_b) <-this is not good but will work. It will be repeated too much
+        Calculator.add_calculation_to_history(subtraction)
+        return Calculator.get_result_of_last_calculation_added_to_history()
+
     @staticmethod
     def multiply_numbers(value_a, value_b):
         """multiply two numbers and store the result"""
-        return value_a * value_b
+        multiplication = Multiplication.create(value_a,value_b)
+        Calculator.add_calculation_to_history(multiplication)
+        return Calculator.get_result_of_last_calculation_added_to_history()
+    @staticmethod
+    def divide_numbers(value_a,value_b):
+        """divide two numbers and store the result"""
+        division = Division.create(value_a,value_b)
+        Calculator.add_calculation_to_history(division)
+        return Calculator.get_result_of_last_calculation_added_to_history()
--- tests/tests.py
+++ tests/tests.py
@@ -1,18 +1,56 @@
 """Testing the Calculator"""
-from calculator.calculator import Calculator
+import pprint
 
-def test_calculator_add():
+import pytest
+
+from calculator.calculator import Calculator
+#this is how you define a function that will run each time you pass it to a test, it is called a fixture
+@pytest.fixture
+def clear_history():
+    Calculator.clear_history()
+
+def test_calculator_add(clear_history):
     """testing the add function of the calculator"""
     assert Calculator.add_number(1,2) == 3
     assert Calculator.add_number(2, 2) == 4
     assert Calculator.add_number(3, 2) == 5
     assert Calculator.add_number(4, 2) == 6
     assert Calculator.history_count() == 4
+    assert Calculator.get_result_of_last_calculation_added_to_history() == 6
+    pprint.pprint(Calculator.history)
 
-def test_calculator_subtract():
+def test_clear_history(clear_history):
+    assert Calculator.add_number(1,2) == 3
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.add_number(4, 2) == 6
+    assert Calculator.history_count() == 4
+    assert Calculator.clear_history() == True
+    assert Calculator.history_count() == 0
+
+def test_count_history(clear_history):
+    assert Calculator.history_count() == 0
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.history_count() == 2
+
+def test_get_last_calculator_result(clear_history):
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.get_result_of_last_calculation_added_to_history() == 5
+
+def test_get_first_calculator_result(clear_history):
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.get_result_of_first_calculation_added_to_history() == 4
+
+def test_calculator_subtract(clear_history):
     """Testing the subtract method of the calculator"""
     assert Calculator.subtract_number(1,2) == -1
 
-def test_calculator_multiply():
+def test_calculator_multiply(clear_history):
     """test multiplication of two numbers"""
     assert Calculator.multiply_numbers(1,2) == 2
+def test_calculator_divide(clear_history):
+    """test division of two numbers"""
+    assert Calculator.divide_numbers(4,2) == 2
